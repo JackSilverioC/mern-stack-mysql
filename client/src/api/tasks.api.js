3f845fa7ac6c3f6b45b2390757d83359159e2be2
Jack Silverio <silveriocieza.jackanibal@gmail.com>
2023-06-10 12:24:36 -0500

    adding env variables

--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -1,21 +1,21 @@
 import axios from "axios";
+import { API_URL } from "../../../server/config";
 
-export const getTasksRequest = async () =>
-  await axios.get("http://localhost:3000/tasks");
+export const getTasksRequest = async () => await axios.get(`${API_URL}/tasks`);
 
 export const createTaskRequest = async (task) =>
-  await axios.post("http://localhost:3000/tasks", task);
+  await axios.post(`${API_URL}/tasks`, task);
 
 export const deleteTaskRequest = async (id) =>
-  await axios.delete(`http://localhost:3000/tasks/${id}`);
+  await axios.delete(`${API_URL}/${id}`);
 
 export const getTaskRequest = async (id) =>
-  await axios.get(`http://localhost:3000/tasks/${id}`);
+  await axios.get(`${API_URL}/tasks/${id}`);
 
 export const updateTaskRequest = async (id, values) =>
-  await axios.put(`http://localhost:3000/tasks/${id}`, values);
+  await axios.put(`${API_URL}/tasks/${id}`, values);
 
 export const toggleTaskDoneRequest = async (id, done) =>
-  await axios.put(`http://localhost:3000/tasks/${id}`, {
+  await axios.put(`${API_URL}/tasks/${id}`, {
     done
   });
--- server/config.js
+++ server/config.js
@@ -1,3 +1,7 @@
+import { config } from "dotenv";
+
+config();
+
 export const PORT = process.env.PORT || 3000;
 
 export const DB_HOST = process.env.DB_HOST || "localhost";
@@ -9,3 +13,5 @@ export const DB_PASSWORD = process.env.DB_PASSWORD || "jackpassword";
 export const DB_NAME = process.env.DB_NAME || "tasksdb";
 
 export const DB_PORT = process.env.DB_PORT || 3306;
+
+export const API_URL = process.env.API_URL || "http://localhost:3000";
